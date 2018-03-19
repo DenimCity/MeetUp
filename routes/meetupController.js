2cbb9c07d679a80843b9babba6f8736e9e71114d
DenimCity <altidorj1@gmail.com>
2018-03-19 15:50:28 -0400

    updated server log for postman testing

--- routes/meetupController.js
+++ routes/meetupController.js
@@ -8,7 +8,7 @@ router.get('/', async (request,response)=> {
   try {
     const meetups = await MeetUp.find({})
     console.log('all created meetups ',meetups)
-    response.json(meetups)
+    response.json({meetups: meetups })
   } catch (error) {
     console.log('error getting meetups ',error)
   }
@@ -18,7 +18,7 @@ router.post('/', async (request,response)=>{
   try {
     const newMeetUp = await MeetUp.create(request.body)
     console.log('new meetup data', newMeetUp);
-    response.json(newMeetUp)
+    response.json({MeetUpCreated: newMeetUp})
     
   } catch (error) {
     response.json('couldnt post route', error)
