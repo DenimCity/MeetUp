81c5fa5298e71d2f91080058812db2e41a566717
DenimCity <altidorj1@gmail.com>
2018-03-21 14:44:16 -0400

    api call works

--- meetup-mobile/constants/api.js
+++ meetup-mobile/constants/api.js
@@ -1,4 +1,14 @@
+import axios from 'axios'
 export const fetchMeetups = () =>{
-  fetch('http://localhost:4000/meetup/').then(res => res.json())
-  console.log('data of respones', fetchMeetups)
+
+  axios
+      .get('http://10.1.6.50:4000/meetup/')
+      .then(response => {
+        const meetups = response.data
+        console.log('data from server ', meetups);
+        return meetups
+      })
+
+  // fetch('http://10.1.6.50:4000/meetup/').then(res => res.json())
+  // console.log('data of respones', fetchMeetups)
 }
