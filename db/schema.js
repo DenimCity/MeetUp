1d5a1f3516dd6e0de5d520fb1b2a39f34d19e2fd
DenimCity <altidorj1@gmail.com>
2018-03-22 16:22:27 -0400

    updated schema, added reqs, and embed metup into groups

--- db/schema.js
+++ db/schema.js
@@ -2,25 +2,43 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 mongoose.Promise = global.Promise
 
-
 const MeetUpSchema = new Schema({
-title:{
-  type:String,
-  required:true
-},
-description:{
-  type:String,
-  required: true
-}
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+    min: [5, 'title must be at least 5 character long']
+  },
+  description: {
+    type: String,
+    required: true,
+    min: [5, "description must be at least"]
+  },
+  eventDate: {
+    type: Date
+  }
+}, {timestamps: true})
 
-
-})
-
-
-
+const GroupSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+    min: [5, "name must be at least  5 charcters long"]
+  },
+  description: {
+    type: String,
+    required: true,
+    min: [10, "Description must be 10 charcters long"]
+  },
+  category: {
+    type: String
+  },
+  meetup: [MeetUpSchema]
+}, {timestamps: true})
 
 module.exports = {
-  MeetUpSchema
-
+  MeetUpSchema,
+  GroupSchema
 
 }
