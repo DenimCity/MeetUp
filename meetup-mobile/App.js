4821cad941739f65dc405a14b84704928b8c6f43
DenimCity <altidorj1@gmail.com>
2018-03-22 19:51:54 -0400

    moving state to the home screen compent

--- meetup-mobile/App.js
+++ meetup-mobile/App.js
@@ -1,43 +1,36 @@
 import React from 'react';
 import {StyleSheet, Text, View, ActivityIndicator} from 'react-native';
-
-import axios from 'axios'
+import EStyleSheet from 'react-native-extended-stylesheet'
+import HomeScreen from './src/screens/HomeScreen'
 export default class App extends React.Component {
  
-  state = {
-    loading: false,
-    meetups: []
-  }
+ 
 
 
-  async componentDidMount() {
-    this.setState({loading: true})
-    const data = await axios.get('http://10.1.6.50:4000/meetup/')
-    .then(response => {
-      const meetups = response.data
-      setTimeout(()=> this.setState({loading: false, meetups}), 300)
-    } )
 
-  }
 
   render() {
 
-    if (this.state.loading) {
-      return (
-        <View style={styles.container}>
-          <ActivityIndicator size="large"/>
-        </View>
-      )
-    }
-    return (
-      <View style={styles.container}>
-        <Text>MeetUp Me !</Text>
-        {this.state.meetups.map((meetup, i)=>{
-          return <Text key={i}>{meetup.title} {meetup.description}</Text>
+return(
+  <HomeScreen />
+)
+    // if (this.state.loading) {
+    //   return (
+    //     <View style={styles.container}>
+    //       <ActivityIndicator size="large"/>
+    //     </View>
+    //   )
+    // }
+    // return (
+    //   <View style={styles.container}>
+    //     <Text>MeetUp Me !</Text>
+    //     {this.state.groups.map((group, i)=>{
+    //       return <Text key={i}>{group.title} {group.description}</Text>
         
         
-        })}</View>
-    );
+    //     })}
+    //     </View>
+    // );
   }
 }
 
