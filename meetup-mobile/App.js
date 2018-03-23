03f70f529b82c9a9a8adfdfb7ed886eac169c25c
DenimCity <altidorj1@gmail.com>
2018-03-23 13:38:54 -0400

    removed unused code

--- meetup-mobile/App.js
+++ meetup-mobile/App.js
@@ -1,44 +1,14 @@
 import React from 'react';
 import {StyleSheet, Text, View, ActivityIndicator} from 'react-native';
-import EStyleSheet from 'react-native-extended-stylesheet'
 import HomeScreen from './src/screens/HomeScreen'
+
+
 export default class App extends React.Component {
- 
- 
-
-
-
 
   render() {
 
-return(
-  <HomeScreen />
-)
-    // if (this.state.loading) {
-    //   return (
-    //     <View style={styles.container}>
-    //       <ActivityIndicator size="large"/>
-    //     </View>
-    //   )
-    // }
-    // return (
-    //   <View style={styles.container}>
-    //     <Text>MeetUp Me !</Text>
-    //     {this.state.groups.map((group, i)=>{
-    //       return <Text key={i}>{group.title} {group.description}</Text>
-        
-        
-    //     })}
-    //     </View>
-    // );
+    return (<HomeScreen/>)
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-});
+
